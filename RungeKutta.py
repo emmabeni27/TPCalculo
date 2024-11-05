7cf7a57210d849fa88173e7fab30c0a56b03ee1c
DecoudJuan <130660498+DecoudJuan@users.noreply.github.com>
2024-11-05 17:05:13 -0300

    Update RungeKutta.py
    runge kutta erased

--- RungeKutta.py
+++ RungeKutta.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+import RungeKutta
+
+
 # Constantes
 G = 6.67430e-11  # Constante gravitacional (m^3 kg^-1 s^-2)
 M = 5.972e24  # Masa de la Tierra (kg)
@@ -20,30 +23,17 @@ def sistema_ecuaciones(t, estado):
     dvthetadt = -vr * vtheta / r
     return np.array([drdt, dthetadt, dvrdt, dvthetadt])
 
-def runge_kutta_4(f, t0, y0, h, num_steps):
-    t = t0
-    y = y0
 
-    for _ in range(num_steps):
-        k1 = h * f(t, y)
-        k2 = h * f(t + h / 2, y + k1 / 2)
-        k3 = h * f(t + h / 2, y + k2 / 2)
-        k4 = h * f(t + h, y + k3)
-        y = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-        t += h
-
-        # Guardar la trayectoria, pero no interrumpir
-    return y
-
-def simular_trayectoria(v0, h=50, num_steps=10000):
+def simular_trayectoria(velocidad_inicial, h=50, num_steps=10000):
+    # Condiciones iniciales
     r0 = 415000000  # Posición radial inicial del satélite (m)
     theta0 = 0  # Posición angular inicial del satélite
-    vr0 = v0  # Velocidad radial inicial
+    vr0 = velocidad_inicial  # Velocidad radial inicial (velocidad de lanzamiento)
     vtheta0 = 0  # Velocidad angular inicial
     estado_inicial = np.array([r0, theta0, vr0, vtheta0])
 
-    # Ejecutar la simulación
-    estado_final = runge_kutta_4(sistema_ecuaciones, 0, estado_inicial, h, num_steps)
+    # Ejecutar la simulación hasta la posición angular del asteroide
+    estado_final = RungeKutta.runge_kutta_4(sistema_ecuaciones, 0, estado_inicial, h, num_steps)
     return estado_final
 
 def verificar_impacto(estado_final, tolerancia_radial=10000):  # tolerancia de 10 km
